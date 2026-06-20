5dbab9648e869eef00483892756702ea12ab2145
Mohammad <m.mohammadimanesh2011@gmail.com>
2026-06-20 22:34:04 +0330

    feat: add markdown rendering in chat messages

--- frontend/src/components/MessageBubble.tsx
+++ frontend/src/components/MessageBubble.tsx
@@ -1,4 +1,5 @@
 import { Message } from '@/types';
+import ReactMarkdown from 'react-markdown';
 
 interface MessageBubbleProps {
   message: Message;
@@ -17,7 +18,9 @@ export default function MessageBubble({ message }: MessageBubbleProps) {
         }`}
         dir="auto"
       >
-        <div className="whitespace-pre-wrap">{message.text}</div>
+        <div className="whitespace-pre-wrap break-words [&>p]:mb-2 [&>p:last-child]:mb-0 [&>ul]:list-disc [&>ul]:pl-5 [&>ol]:list-decimal [&>ol]:pl-5 [&>pre]:bg-black/10 [&>pre]:dark:bg-white/10 [&>pre]:p-2 [&>pre]:rounded-md [&>code]:bg-black/10 [&>code]:dark:bg-white/10 [&>code]:px-1 [&>code]:rounded [&>h1]:text-lg [&>h1]:font-bold [&>h2]:text-base [&>h2]:font-bold [&>h3]:font-bold">
+          <ReactMarkdown>{message.text}</ReactMarkdown>
+        </div>
       </div>
     </div>
   );
